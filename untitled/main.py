17db8e4e55b8cb66026ade45d4db3752b6fe2afe
Andrew Ellen <18004410@uhi.ac.uk>
2024-06-10 07:57:46 +0100

    restored the code to be less overkill since it did nothing to improve it

--- untitled/main.py
+++ untitled/main.py
@@ -14,28 +14,22 @@ def train_model():
     # Flatten turns it into a flat layer instead of a grid of 28x28 pixels. So one big line of 784 pixels.
     # model.add(tf.keras.layers.Flatten(input_shape(28,28)))
     # The one in "(28, 28, 1)" is required or else it'll error out
-    model.add(tf.keras.layers.Conv2D(32, (3,3), activation='sigmoid', input_shape=(28, 28, 1)))
-
-    model.add(tf.keras.layers.MaxPool2D((2, 2)))
-    model.add(tf.keras.layers.Conv2D(48, (3,3), activation='sigmoid'))
-    model.add(tf.keras.layers.MaxPool2D((2, 2)))
-    model.add(tf.keras.layers.Dropout(0.5))
-    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
 
     # A dense layer is the most basic layer. In a dense layer each neuron is connected to each other neuron of the other layer.
     # relu is rectified linear unit
     # softmax is essentially the confidence value for the output which is the probability of the digit being the right number.
-    model.add(tf.keras.layers.Dense(512, activation='sigmoid'))
-    model.add(tf.keras.layers.Dense(512, activation='sigmoid'))
+    model.add(tf.keras.layers.Dense(128, activation='relu'))
+    model.add(tf.keras.layers.Dense(128, activation='relu'))
     # ten is the number of neurons. Only 10 are needed as there are only ten digits
-    #model.add(tf.keras.layers.Dense(10, activation='softmax'))
-    model.add(tf.keras.layers.Dense(10, activation='sigmoid'))
+    # model.add(tf.keras.layers.Dense(10, activation='softmax'))
+    model.add(tf.keras.layers.Dense(10, activation='softmax'))
 
     # Compiling the model
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
     # Fitting the model (training it). Epochs are how many times it runs the training
-    model.fit(x_training_data, y_training_data, epochs=500)
+    model.fit(x_training_data, y_training_data, epochs=10)
 
     # Saving the model
     model.save('handwritten_number_recognition_model.keras')
@@ -73,24 +67,27 @@ def run_model():
 
 
 if __name__ == "__main__":
-    # Setting up global variables for training data and the model
+    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+    print(tf.test.is_built_with_cuda())
+    if tf.test.is_built_with_cuda():
+        # Setting up global variables for training data and the model
 
-    # The MNIST labelled dataset for handwritten numbers
-    global mnist
-    mnist = tf.keras.datasets.mnist
+        # The MNIST labelled dataset for handwritten numbers
+        global mnist
+        mnist = tf.keras.datasets.mnist
 
-    # x_training_data is the image data. y_training_data is the labelled data.
-    # x_test_data is the test image data. y_test_data is the test labelled data.
-    global x_training_data, y_training_data, x_test_data, y_test_data
-    (x_training_data, y_training_data), (x_test_data, y_test_data) = mnist.load_data()
+        # x_training_data is the image data. y_training_data is the labelled data.
+        # x_test_data is the test image data. y_test_data is the test labelled data.
+        global x_training_data, y_training_data, x_test_data, y_test_data
+        (x_training_data, y_training_data), (x_test_data, y_test_data) = mnist.load_data()
 
-    # Normalizing the training and test data.
-    x_training_data = tf.keras.utils.normalize(x_training_data, axis=1)
-    x_test_data = tf.keras.utils.normalize(x_test_data, axis=1)
+        # Normalizing the training and test data.
+        x_training_data = tf.keras.utils.normalize(x_training_data, axis=1)
+        x_test_data = tf.keras.utils.normalize(x_test_data, axis=1)
 
-    # If model doesn't exist then train it
-    if not os.path.isfile('handwritten_number_recognition_model.keras'):
-        train_model()
-    global model
-    model = tf.keras.models.load_model('handwritten_number_recognition_model.keras')
-    run_model()
+        # If model doesn't exist then train it
+        if not os.path.isfile('handwritten_number_recognition_model.keras'):
+            train_model()
+        global model
+        model = tf.keras.models.load_model('handwritten_number_recognition_model.keras')
+        run_model()
